953c2fb75694352ef7722b4f27273baf93aaac4a
Jonathan Tri <jonathantric@gmail.com>
2023-02-09 11:14:06 +0700

    add lecture3

---
+++ Lecture3/Memorize/Memorize/ContentView.swift
@@ -0,0 +1,92 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by JonathanTriC on 31/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var emojis  = ["✈️", "🚗", "🚂", "🚀", "🚁", "🚒", "🛵", "🚕", "🛳️", "🚔", "🛻", "🏎️", "🚑", "🚲", "🚘", "🚛", "🚙", "🚚", "🏍️", "🛶", "⛴️", "🚤", "🚎", "🚖"]
+    @State var emojiCount = 4
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer(minLength: 20)
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal )
+        }
+        .padding(.horizontal)
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 4 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        //            .preferredColorScheme(.dark)
+        //        ContentView()
+        //            .preferredColorScheme(.light)
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp: Bool = false
+    var content: String
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            
+            if isFaceUp {
+                shape
+                    .fill()
+                    .foregroundColor(.white)
+                shape
+                    .strokeBorder(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                shape
+                    .fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
